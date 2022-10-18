f56e871801ddc53ad51e8fb615852e2cbb170722
Vu Le Bao Long <vulebaolong@gmail.com>
2022-10-19 05:58:24 +0700

    done 2 dispay drag new

--- test/test2.js
+++ test/test2.js
@@ -503,8 +503,8 @@ function run() {
                 let eMove_blx = eMove_bl.x
 
                 if (eMove_bly >= condition_top_height) {
-                    el_screenshot_bottom.style.height = el_screenshot_wrapper_height - eMove_bly + 'px'                    
-                    el_screenshot_right.style.height = eMove_bly + 'px'                    
+                    el_screenshot_bottom.style.height = el_screenshot_wrapper_height - eMove_bly + 'px'
+                    el_screenshot_right.style.height = eMove_bly + 'px'
                     el_screenshot_center.style.height = eMove_bly - el_screenshot_top_height + 'px'
                 }
                 if (eMove_blx <= condition_left_width) {
@@ -590,12 +590,118 @@ function fun_handle_click_chat_end(mess_chat) {
 
 
 
+let el_toolbar_wraper = document.querySelector('.toolbar_wraper')
 let el_toolbar_left = document.querySelector('.toolbar_left')
+let el_toolbar_container = document.querySelector('.toolbar_container')
+let el_toolbar_right_collapse = document.querySelector('.toolbar_right_collapse')
+let el_time_time = document.querySelector('.toolbar_wraper > .toolbar_left > .time_time')
+console.log(el_time_time);
 
-el_toolbar_left.onclick = () => {
-    let el_toolbar_container = document.querySelector('.toolbar_container')
-    el_toolbar_container.classList.toggle('toolbar_container_show')
+el_time_time.onclick = (e) => {
+    e.stopPropagation()
+    // el_time_time.onmousedown = (e1) => { e1.stopPropagation() }
+    // el_time_time.onmousemove = (e2) => { e2.stopPropagation() }
+    // el_time_time.onmouseup = (e3) => { e3.stopPropagation() }
+    let el_toolbar_container_left = el_toolbar_container.offsetLeft
+    if (e.altKey) {
+        console.log(el_toolbar_container_left);
+        if (el_toolbar_container_left === 60) {
+            
+            el_toolbar_container.style.cssText = `
+            left: unset;
+            right: -20px;
+            justify-content: start;
+            `;
+            el_toolbar_left.style.cssText = `
+            justify-content: end;
+            `;
+            
+            el_toolbar_container.appendChild(el_toolbar_container.firstElementChild);
+            el_toolbar_left.appendChild(el_toolbar_left.firstElementChild);
+            el_toolbar_right_collapse.innerHTML = `<i class="fa-solid fa-chevron-right"></i>`
+        }else {
+            el_toolbar_container.style.cssText = `
+            left: calc(80px - 20px);
+            right: unset;
+            justify-content: end;
+            `;
+
+            el_toolbar_left.style.cssText = `
+            justify-content: start;
+            `;
+            
+            el_toolbar_container.appendChild(el_toolbar_container.firstElementChild);
+            el_toolbar_left.appendChild(el_toolbar_left.firstElementChild);
+            
+            el_toolbar_right_collapse.innerHTML = `<i class="fa-solid fa-chevron-left"></i>`
+        }
+    } else {
+        console.log(el_toolbar_container_left);
+
+        el_toolbar_container.classList.toggle('toolbar_container_show')
+    }
 }
 
+dragElement(el_toolbar_wraper)
 
+let el_ar = [
+    'toolbar_container',
+    'time_time',
+    
+]
 
+el_ar.forEach((e) => {
+    let element = document.querySelector(`.${e}`)
+    // element.onclick = (e1) => {e1.stopPropagation()}
+    element.onmousedown = (e1) => { e1.stopPropagation() }
+    element.onmousemove = (e1) => { e1.stopPropagation() }
+    element.onmouseup = (e1) => { e1.stopPropagation() }
+    if (element.getAttribute("type") === 'text') {
+        element.onclick = (e) => {
+            element.select()
+        }
+    }
+}) 
+function dragElement(elmnt) {
+    var pos1 = 0,
+        pos2 = 0,
+        pos3 = 0,
+        pos4 = 0;
+    if (document.getElementById(elmnt.id + 'header')) {
+        /* if present, the header is where you move the DIV from:*/
+        document.getElementById(elmnt.id + 'header').onmousedown = dragMouseDown;
+    } else {
+        /* otherwise, move the DIV from anywhere inside the DIV:*/
+        elmnt.onmousedown = dragMouseDown;
+    }
+
+    function dragMouseDown(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // get the mouse cursor position at startup:
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        document.onmouseup = closeDragElement;
+        // call a function whenever the cursor moves:
+        document.onmousemove = elementDrag;
+    }
+
+    function elementDrag(e) {
+        e = e || window.event;
+        e.preventDefault();
+        // calculate the new cursor position:
+        pos1 = pos3 - e.clientX;
+        pos2 = pos4 - e.clientY;
+        pos3 = e.clientX;
+        pos4 = e.clientY;
+        // set the element's new position:
+        elmnt.style.top = elmnt.offsetTop - pos2 + 'px';
+        elmnt.style.left = elmnt.offsetLeft - pos1 + 'px';
+    }
+
+    function closeDragElement() {
+        /* stop moving when mouse button is released:*/
+        document.onmouseup = null;
+        document.onmousemove = null;
+    }
+}
